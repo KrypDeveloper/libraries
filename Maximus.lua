5e55eb999be857bf24fb30d56a829def98b49c12
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-02-28 14:10:14 -0300

    Update Maximus.lua

--- Maximus.lua
+++ Maximus.lua
@@ -1,9 +1,10 @@
 Lib = {}
 function Lib.TeleportPlayer(x, y, z)
-  local XYZ = {x, y, z}
-  while game.Players.LocalPlayer.Character.HumanoidRootPart.Position =~ unpack(XYZ) do
+ local XYZ = {x, y, z}
+ while game.Players.LocalPlayer.Character.HumanoidRootPart.Position =~ unpack(XYZ) do
   game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(x, y, z))
-  end
+ end
+end
 
   function Lib.TeleportPlace(id)
     game:GetService("TeleportService"):Teleport(id)

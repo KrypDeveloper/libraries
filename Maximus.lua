c6b78251c1e927cb2464534afa1e035c53a11183
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-03-04 12:52:06 -0300

    Update Maximus.lua

--- Maximus.lua
+++ Maximus.lua
@@ -1,10 +1,26 @@
-Lib = {}
+Lib = {
+  state = true
+}
+
 function Lib.TeleportPlayer(x, y, z)
   game.Players.LocalPlayer.Character:SetPrimaryPartCFrame(CFrame.new(x, y, z))
 end
 
-  function Lib.TeleportPlace(id)
+function Lib.TeleportPlace(id)
     game:GetService("TeleportService"):Teleport(id)
-  end
+end
+
+function Lib.Desable()
+  Lib.state = false
+end
+
+function Lib.Active()
+  Lib
+  State = true
+end
+
+function Lib.KillPlayer()
+  game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid").Health = 0
+end
 
 return Lib

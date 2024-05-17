a61c6b7bb71add6d7bdd0f25bb45b70538486bbf
krypton <71219830+KrypDeveloper@users.noreply.github.com>
2024-05-17 09:54:03 -0300

    Update example.lua

--- Cosmos/example.lua
+++ Cosmos/example.lua
@@ -3,30 +3,29 @@ local Cosmos = loadstring(
     game:HttpGet("https://raw.githubusercontent.com/KrypDeveloper/MAXIMUS-LIBRARY/main/Cosmos/source.luau")
 ))()
 
-local MyElement = Cosmos:Make({
-    ClassName = "Part",
+local MyElement = Cosmos:Make("Part",{
     Size = Vector3.new(4, 1, 2),
     Color = Color3.fromRGB(255, 0, 0)
 })
 
-MyElement().Parent = workspace
+MyElement.Parent = workspace
 
 --Making UI
 
-local ScreenGui = Cosmos:Make({
-    ClassName = "ScreenGui"
+local ScreenGui = Cosmos:Make("Part",{
+    Name = "Part"
 })
 
-ScreenGui().Parent = gethui()
-local Canvas = Cosmos:Make({
-    ClassName = "CanvasGroup",
+ScreenGui.Parent = gethui()
+local Canvas = Cosmos:Make("CanvasGroup",{
     Size = UDim2.new(0,300,0,200),
     Position = UDim2.new(0,260,0,90),
     BackgroundColor3 = Color3.fromRGB(22,22,29),
 })
 
-Canvas().Parent = ScreenGui()
-local UiCorner = Cosmos:Make({
-    ClassName = "UICorner",
+Canvas.Parent = ScreenGui()
+local UiCorner = Cosmos:Make("UICorner", {
+    Name = 'UICorner'
 })
-UiCorner().Parent = Canvas()
+
+UiCorner.Parent = Canvas()
